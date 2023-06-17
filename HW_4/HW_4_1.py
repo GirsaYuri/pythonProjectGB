868ca925ae4d20b1e1a63aa1bde11807e2e7b086
GirsaYuri <87301951+GirsaYuri@users.noreply.github.com>
2023-06-17 15:27:29 +0700

    Update HW_4_1.py

--- HW_4/HW_4_1.py
+++ HW_4/HW_4_1.py
@@ -1,3 +1,4 @@
+# Напишите функцию для транспонирования матрицы
 matrix = [[1,2], [3 ,4], [5 ,6]]
 # trans_matrix = [[0 for j in range(len(matrix))] for i in range(len(matrix[0]))]
 # print (trans_matrix)
